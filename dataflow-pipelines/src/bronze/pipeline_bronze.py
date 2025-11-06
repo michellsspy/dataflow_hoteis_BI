162c952104a3ed34bdc6bdc2186995be1f4c7f5a
michellsspy <michellss.py@gmail.com>
2025-11-06 19:15:19 -0300

    Nova arquitetura criada para a camada Bronze

--- dataflow-pipelines/src/bronze/pipeline_bronze.py
+++ dataflow-pipelines/src/bronze/pipeline_bronze.py
@@ -1,40 +1,54 @@
 import apache_beam as beam
 from apache_beam.options.pipeline_options import PipelineOptions
 # Mude a importação para a classe PipelineOptions, não a função de dicionário
-from .classes.bronze_options import BronzePipelineOptions 
-from .classes.transforms_bq import GetFolderNames, GcsCsvToBqUpsert
-from .functions.utils import setup_logging
-from .schemas.bronze_schemas import SCHEMA_MAP, PK_COLS_bronze
+from classes.bronze_options import BronzePipelineOptions 
+from classes.transforms_bq import GetFolderNames, GcsCsvToBqUpsert
+from functions.utils import setup_logging
 from datetime import datetime
 import sys
+from datetime import datetime
 
 logger = setup_logging()
 
-def run_pipeline(argv=None):
+data_now = datetime.now().strftime("%Y%m%d%H%M")
+
+def run_pipeline():
+    data_now = datetime.now().strftime("%Y%m%d%H%M")
+    argv = sys.argv
+    options = PipelineOptions(
+        project='etl-hoteis',
+        runner='DataflowRunner',
+        streaming=False,
+        job_name=f"etl-transient-to-bronze-{data_now}",
+        temp_location='gs://bk-etl-hotelaria/temp',
+        staging_location='gs://bk-etl-hotelaria/staging',
+        #template_location=f'gs://bk-etl-hotelaria/templates/etl-transient-to-bronze-{data_now}',
+        autoscaling_algorithm='THROUGHPUT_BASED',
+        worker_machine_type='n1-standard-4',
+        num_workers=1,
+        max_num_workers=3,
+        disk_size_gb=25,
+        region='us-central1',
+        #zone='us-central1-c',
+        #worker_zone='us-central1-a',
+        project_id='etl-hoteis',
+        staging_bucket='bk-etl-hotelaria',
+        save_main_session=False,
+        #experiments='use_runner_v2',
+        prebuild_sdk_container_engine='cloud_build',
+        docker_registry_push_url='us-central1-docker.pkg.dev/etl-hoteis/dataflow/bronze-hotelaria',
+        sdk_container_image='us-central1-docker.pkg.dev/etl-hoteis/dataflow/bronze-hotelaria:latest',
+        sdk_location='container',
+        requirements_file='./requirements.txt',
+        metabase_file='./metadata.json',
+        setup_file='./setup.py',
+        service_account_email='etl-hoteis@etl-hoteis.iam.gserviceaccount.com'
+    )
+
     """Ponto de entrada principal para o pipeline da camada Bronze (bronze)."""
     
-    # 1. Configuração de Opções
-    # O PipelineOptions usa sys.argv por padrão. Ao passar argv=None, ele
-    # lê automaticamente os argumentos da linha de comando (CLI ou Flex Template).
-    # Passamos `options=BronzePipelineOptions(argv)` para que o BEAM
-    # processe TODOS os argumentos da CLI E aplique os defaults da classe.
-    options = BronzePipelineOptions(argv)
-    
     # Configurações dinâmicas (que não podem ser setadas via argparse simples)
-    data_now = datetime.now().strftime("%Y%m%d%H%M")
-    options.view_as(PipelineOptions).job_name = f"etl-bronze-bronze-hotelaria-{data_now}"
     
-    # Configurações Fixas de Infraestrutura/Runner (se não quiser que venham da CLI)
-    options.view_as(PipelineOptions).runner = 'DataflowRunner'
-    options.view_as(PipelineOptions).streaming = False
-    options.view_as(PipelineOptions).temp_location = 'gs://bk-etl-hotelaria/temp'
-    options.view_as(PipelineOptions).staging_location = 'gs://bk-etl-hotelaria/staging'
-    options.view_as(PipelineOptions).template_location = 'gs://bk-etl-hotelaria/templates/template-etl-bronze.json'
-
-    # Se a Service Account for sempre a mesma:
-    options.view_as(PipelineOptions).service_account_email = 'etl-hoteis@etl-hoteis.iam.gserviceaccount.com'
-
-    # Acessa as opções customizadas de forma CORRETA
     bronze_options = options.view_as(BronzePipelineOptions)
     
     logger.info("Iniciando pipeline Bronze (GCS Transient -> BQ bronze Upsert)")
@@ -63,9 +77,7 @@ def run_pipeline(argv=None):
                     projeto=bronze_options.project, # 'project' veio da classe BaseOptions
                     dataset_id=bronze_options.dataset_id,
                     bucket_name=bronze_options.gcs_bucket_name,
-                    gcs_prefix=bronze_options.gcs_transient_prefix,
-                    schema_map=SCHEMA_MAP,
-                    pk_map=PK_COLS_bronze
+                    gcs_prefix=bronze_options.gcs_transient_prefix
                 )
             ).with_outputs(
                 GcsCsvToBqUpsert.TAG_FAILED,
