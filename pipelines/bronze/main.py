7563a86cb2e2eb884c1305f2a2edc7f58407854c
michellsspy <michellss.py@gmail.com>
2025-11-07 11:27:13 -0300

    Criado e testado o Dockerfile

--- pipelines/bronze/main.py
+++ pipelines/bronze/main.py
@@ -1,27 +1,14 @@
-# main_transient_to_bronze.py
-
 from apache_beam.options.pipeline_options import PipelineOptions
-from datetime import datetime
+from google.cloud import secretmanager
+from google.cloud import storage
+from datetime import datetime, timedelta
 import apache_beam as beam
+import tempfile
 import logging
 import sys
 import os
 
-# Importando Classes e Funções dos Helpers
-from classes.get_tables_names import GetTablesName
-from classes.write_to_bq import WriteToBQ 
-from functions.bq_gcs_helpers import PROJETO_DESTINO, REGION_ID 
-from google.cloud import bigquery
-
-# --- Configuração de Logging ---
-logging.basicConfig(
-    level=logging.INFO,
-    stream=sys.stdout,
-    format="%(asctime)s - %(levelname)s - [%(name)s] - (%(filename)s:%(lineno)d) - %(funcName)s - %(message)s",
-    datefmt="%Y-%m-%d %H:%M:%S",
-)
-logger = logging.getLogger(__name__)
-
+# Importando as Funções
 from functions.get_secret import get_secret, save_secret_to_temp_file
 
 # Recupera a chave e salva temporariamente
@@ -31,42 +18,49 @@ key_path = save_secret_to_temp_file(key_data)
 # Configura as credenciais do GCP
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = key_path
 
-def main_transient_to_bronze():
-    data_now = datetime.now().strftime("%y%m%d")
-    
-    # --- Configurações Dataflow (Adaptado do seu código) ---
+def main_transient_to_raw():
+    data_now = datetime.now().strftime("%Y%m%d")
+    argv = sys.argv
     options = PipelineOptions(
-        project='prosite-455612', # Projecto de execução (onde os workers rodam)
-        runner='DataflowRunner',
-        streaming=False,
-        job_name=f"etl-hoteis-bronze-{data_now}",
-        temp_location='gs://site_prosite/temp',
-        staging_location='gs://site_prosite/staging',
-        region=REGION_ID, # Usa a REGION_ID definida no helper (southamerica-east1)
-        # ... (Outras configurações de worker) ...
+        project = 'etl-hoteis',
+        runner = 'DataflowRunner',
+        streaming = False,
+        job_name = f"etl-hotelaria-{data_now}",
+        temp_location = 'gs://bk-etl-hotelaria/temp',
+        staging_location = 'gs://bk-etl-hotelaria/staging',
+        #template_location = f'gs://bk-etl-hotelaria/templates/etl-hotelaria-{data_now}',
+        autoscaling_algorithm = 'THROUGHPUT_BASED',
+        worker_machine_type = 'n1-standard-4',
+        num_workers = 1,
+        max_num_workers = 3,
+        disk_size_gb = 25,
+        region = 'us-central1',
+        #zone = 'us-central1-c',
+        #worker_zone = 'us-central1-a',
+        project_id = 'etl-hoteis',
+        staging_bucket = 'bk-etl-hotelaria',
+        save_main_session = False,
+        #experiments = 'use_runner_v2',
+        prebuild_sdk_container_engine = 'cloud_build',
+        docker_registry_push_url = 'us-central1-docker.pkg.dev/etl-hoteis/etl-hotelaria/hotelaria-dev',
+        sdk_container_image = 'us-central1-docker.pkg.dev/etl-hoteis/etl-hotelaria/hotelaria-dev:latest',
+        sdk_location = 'container',
+        requirements_file = './tx/requirements.txt',
+        metabase_file = './metadata.json',
+        setup_file = './setup.py',
+        service_account_email = 'etl-743@etl-hoteis.iam.gserviceaccount.com'
     )
     
-    # 🚨 PREVENÇÃO: Garante que o dataset de destino existe (Opção 2) 🚨
-    # Usei o dataset DATASET_DESTINO (bronze_hotelaria) nos helpers.
-    # É fundamental que o dataset destino já exista, pois o MERGE não o cria.
-    
-    # Não vamos adicionar a função de criação de dataset aqui, para manter o foco
-    # no pipeline. Assumimos que o dataset bronze_hotelaria existe.
-    
-    with beam.Pipeline() as p:
-            # 1. Listar Nomes das Tabelas
+    with beam.Pipeline(options=options) as p:
             tables = (
                 p
                 | "Start PCollection" >> beam.Create([None])
-                | "Get Tables Names" >> beam.ParDo(GetTablesName()) # PCollection de Nomes
+                | "Get Tables Names" >> beam.ParDo(GetTablesName())
             )
 
-            # 2. Processar UPSERT e Carregar no BigQuery
+            # Processo para copiar e salvar como Parquet
             (
                 tables
-                | "Processar e Carregar BQ" >> beam.ParDo(WriteToBQ()) # Executa LoadJob + MERGE + Limpeza
-                | "Exibir Resultados" >> beam.Map(logger.info) # Imprime o status UPSERT OK ou ERRO
+                | "Processar Tabelas do BigQuery" >> beam.ParDo(BQToParquet())
+                | "Exibir Resultados" >> beam.Map(print)
             )
-        
-if __name__ == '__main__':
-    main_transient_to_bronze()
