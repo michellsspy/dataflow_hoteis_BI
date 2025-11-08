9b19f1162d7bf24e42a8c377b10c4421252046ab
michellsspy <michellss.py@gmail.com>
2025-11-08 13:07:00 -0300

    Camada bronze finalizada

--- pipelines/bronze/setup.py
+++ pipelines/bronze/setup.py
@@ -1,27 +1,15 @@
-# src/bronze/setup.py
+from setuptools import setup, find_packages
 
-import setuptools
-
-# A pasta raiz do projeto (dataflow-pipelines) é o diretório de contexto do Docker/Cloud Build.
-# O Dockerfile copia 'src' para o WORKDIR.
-# O pacote é 'bronze'.
-
-setuptools.setup(
-    name='dataflow-hotelaria',
-    version='1.0.0',
-    packages=setuptools.find_packages(where='src'),
-    package_dir={'': 'src'},
-    install_requires=[
-        # Dependências de runtime. Geralmente, você pode deixar o requirements.txt cuidar disso,
-        # mas incluir o beam aqui garante a instalação do SDK.
-        'apache-beam[gcp]>=2.56.0',
-        'pytest',
-    ],
-    # Pacotes de dependência
-    # Aqui listamos os pacotes que o Dataflow deve empacotar.
-    # Garantimos que 'bronze' seja incluído, permitindo imports relativos.
+setup(
+    name="bronze_pipeline",
+    version="0.1",
+    packages=find_packages(),  # procura pacotes no diretório atual (bronze/)
     include_package_data=True,
-    description='Pipeline Apache Beam para ingestão da Camada Bronze da rede hoteleira.',
-    author='Michel Santana',
-    license='MIT'
+    install_requires=[
+        "apache-beam[gcp]",
+        "google-cloud-bigquery",
+        "google-cloud-storage",
+        "pandas",
+        "pyarrow"
+    ],
 )
