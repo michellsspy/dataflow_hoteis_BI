4d913125a60e2c76a2e2f451149236df4d1f0976
michellsspy <michellss.py@gmail.com>
2025-11-08 13:22:05 -0300

    Camada bronze finalizada

--- pipelines/bronze/setup.py
+++ pipelines/bronze/setup.py
@@ -1,15 +1,20 @@
-from setuptools import setup, find_packages
+import setuptools
+import sys
 
-setup(
-    name="bronze_pipeline",
-    version="0.1",
-    packages=find_packages(),  # procura pacotes no diretório atual (bronze/)
-    include_package_data=True,
+# Garante que o setuptools seja usado para empacotamento
+setuptools.setup(
+    name='hotelaria-etl',
+    version='0.0.1',
     install_requires=[
-        "apache-beam[gcp]",
-        "google-cloud-bigquery",
-        "google-cloud-storage",
-        "pandas",
-        "pyarrow"
+        # Se você usa um requirements.txt, este campo é opcional,
+        # mas inclua dependências críticas aqui, como google-cloud-bigquery
+        'google-cloud-bigquery',
+        'apache-beam[gcp]', 
+        'pandas',
+        'pyarrow',
+        # ...
     ],
+    packages=setuptools.find_packages(where='.'),
+    # Define o diretório raiz para a busca de pacotes
+    package_dir={'': '.'},
 )
